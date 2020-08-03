77fb9024f1d5c7db2bc20adef1555984ea195a55
alexey1986 <aliaksey.poleshchuk.epam@gmail.com>
2020-08-03 12:18:01 +0300

    create multiselect

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './dropdown.module.css';
-import {ReactComponent as ReactIcon} from './icon.svg';
+import { ReactComponent as ReactIcon } from './icon.svg';
 
 export default class Dropdown extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            displayMenu: false
+            displayMenu: false,
+            selectedValues: []
         };
 
-        this.setWrapperRef = this.setWrapperRef.bind(this);
         this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+        this.showDropdownMenu = this.showDropdownMenu.bind(this);
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+    }
+
+    componentDidMount() {
+        const { selected } = this.props;
+
+        this.setState({ selectedValues: [selected.value] })
     }
 
     setWrapperRef(node) {
@@ -31,20 +40,35 @@ export default class Dropdown extends React.Component {
         });
     }
 
-    hideDropdownMenu() {
+    hideDropdownMenu(){
         this.setState({ displayMenu: false }, () => {
             document.removeEventListener('click', this.handleClickOutside);
         });
     }
 
+    handleChange = (e) => {
+        const { selectedValues } = this.state;
+        const currentValue = e.target.value;
+        const currentItem = selectedValues.find(value => value === currentValue);
+
+        if (currentItem) {
+            const filtered = selectedValues.filter(value => value !== currentValue)
+
+            this.setState({ selectedValues: filtered });
+        } else {
+            this.setState({ selectedValues: [...selectedValues, currentValue] });
+        }
+    }
+
     render() {
-        const { options, selected } = this.props;
+        const { options } = this.props;
+        const { selectedValues } = this.state;
 
         const NumberList = () => {
             const listItems = options.map((item, i) =>
                 <li className={styles.item} key={i}>
-                    <input className={styles.checkbox} id={`checkbox_${i}`} type="checkbox" />
-                    <label className={styles.label} for={`checkbox_${i}`}>
+                    <input className={styles.checkbox} id={`checkbox_${i}`} value={item.value} checked={selectedValues.includes(item.value)} type="checkbox" onChange={this.handleChange} />
+                    <label className={styles.label} htmlFor={`checkbox_${i}`}>
                         {item.value}
                     </label>
                 </li>
@@ -57,7 +81,7 @@ export default class Dropdown extends React.Component {
         return (
             <div className={`${styles.dropdown} ${this.state.displayMenu ? `${styles.active}` : ''}`}>
                 <div className={styles.button} onClick={(e) => this.showDropdownMenu(e)}>
-                    {selected.label}
+                    <div>{selectedValues.length ? selectedValues.map((item, i) => <span key={i}>{item}</span>) : 'Select'}</div>
                     <ReactIcon className={styles.icon} />
                 </div>
                 {this.state.displayMenu ? (
